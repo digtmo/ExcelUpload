aa94650ad240329a37e160d82451f3047b89877f
David Torres <davidtorresim@gmail.com>
2023-01-26 12:27:59 -0300

    Browse en ingles

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { read, utils, writeFile } from 'xlsx';
 
 const HomeComponent = () => {
-    const [movies, setMovies] = useState([]);
+    const [dataCliente, setdataCliente] = useState([]);
 
     const handleImport = ($event) => {
         const files = $event.target.files;
@@ -15,7 +15,7 @@ const HomeComponent = () => {
 
                 if (sheets.length) {
                     const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
-                    setMovies(rows)
+                    setdataCliente(rows)
                 }
             }
             reader.readAsArrayBuffer(file);
@@ -24,17 +24,21 @@ const HomeComponent = () => {
 
     const handleExport = () => {
         const headings = [[
-            'Movie',
-            'Category',
-            'Director',
-            'Rating'
+            'Empresa',
+            'Encargado',
+            'Curso',
+            'Inscritos',
+            'Valor',
+            "Nombre",
+            "Rut",
+            "Correo",
         ]];
         const wb = utils.book_new();
         const ws = utils.json_to_sheet([]);
         utils.sheet_add_aoa(ws, headings);
-        utils.sheet_add_json(ws, movies, { origin: 'A2', skipHeader: true });
+        utils.sheet_add_json(ws, dataCliente, { origin: 'A2', skipHeader: true });
         utils.book_append_sheet(wb, ws, 'Report');
-        writeFile(wb, 'Movie Report.xlsx');
+        writeFile(wb, 'dataCliente Report.xlsx');
     }
 
     return (
@@ -43,11 +47,10 @@ const HomeComponent = () => {
                 <div className="col-sm-6 offset-3">
                     <div className="row">
                         <div className="col-md-6">
-                            <div className="input-group">
+                            <div className="input-group">                
                                 <div className="custom-file">
-                                    <input type="file" name="file" className="custom-file-input" id="inputGroupFile" required onChange={handleImport}
-                                        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/>
-                                    <label className="custom-file-label" htmlFor="inputGroupFile">Choose file</label>
+                                    <input type="file" name="file" className="custom-file-input" id="customFileLang" required onChange={handleImport} accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" lang="es"/>
+                                    <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
                                 </div>
                             </div>
                         </div>
@@ -64,29 +67,62 @@ const HomeComponent = () => {
                     <table className="table">
                         <thead>
                             <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">Movie</th>
-                                <th scope="col">Category</th>
-                                <th scope="col">Director</th>
-                                <th scope="col">Rating</th>
+                                <th scope="col">Empresa</th>
+                                <th scope="col">Encargado</th>
+                                <th scope="col">Nombre Curso</th>
+                                <th scope="col">Inscritos</th>
+                                <th scope="col">Valor</th>
                             </tr>
                         </thead>
                         <tbody> 
-                                {
-                                    movies.length
+                        {
+                                    dataCliente.length
                                     ?
-                                    movies.map((movie, index) => (
-                                        <tr key={index}>
-                                            <th scope="row">{ index + 1 }</th>
-                                            <td>{ movie.Movie }</td>
-                                            <td>{ movie.Category }</td>
-                                            <td>{ movie.Director }</td>
-                                            <td><span className="badge bg-warning text-dark">{ movie.Rating }</span></td>
+                                    dataCliente.map((movie) => (
+                                        <tr>
+                                            <th>{ movie.Empresa }</th>
+                                            <td>{ movie.Encargado }</td>
+                                            <td>{ movie.Curso }</td>
+                                            <td>{ movie.Inscritos }</td>
+                                            <td>{ movie.Valor }</td>
                                         </tr> 
                                     ))
                                     :
                                     <tr>
-                                        <td colSpan="5" className="text-center">No Movies Found.</td>
+                                        <td colSpan="5" className="text-center">No dataCliente Found.</td>
+                                    </tr> 
+                                }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-6 offset-3">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Rut</th>
+                                <th scope="col">Correo</th>
+                            </tr>
+                        </thead>
+                        <tbody> 
+                               
+                                 {
+                                    dataCliente.length
+                                    ?
+                                    dataCliente.map((movie, index) => (
+                                        <tr key={index}>
+                                            <th scope="row">{ index + 1 }</th>
+                                            <td>{ movie.Nombre }</td>
+                                            <td>{ movie.Rut }</td>
+                                            <td>{ movie.Correo }</td>
+                                        </tr> 
+                                    ))
+                                    :
+                                    <tr>
+                                        <td colSpan="5" className="text-center">No dataCliente Found.</td>
                                     </tr> 
                                 }
                         </tbody>
